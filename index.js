acc79108d468e675b9f0bcef141cdd4453b6fa9f
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-12 15:13:26 +0600

    add endpoint to delete a plant by seller

--- index.js
+++ index.js
@@ -153,6 +153,13 @@ async function run() {
       res.send(result)
     })
 
+    // delete a plant by seller 
+    app.delete('/plants/:id', verifyToken, verifySeller, async (req, res) => {
+      const id = req.params.id;
+      const result = await plantsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
+
     // get all users 
     app.get('/all-users/:email', verifyToken, verifyAdmin, async (req, res) => {
       const email = req.params.email;
